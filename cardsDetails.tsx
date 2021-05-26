40d4e1baa182ff0fbde64e169c9feba928bba36d
jitendra19 <jiten.cs2010@gmail.com>
2021-05-26 18:13:41 +0530

    1 file modified

--- cardsDetails.tsx
+++ cardsDetails.tsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import axios from 'axios';
 
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 export default class Card extends Component {
   constructor() {
@@ -29,20 +32,26 @@ export default class Card extends Component {
   }
 
   enteredUserName = React.createRef();
+
+  columnDefs = [
+    { headerName: 'avatar_url', 
+      field: 'avatar_url',
+      cellRenderer: (params)=>`<img src={${params.data.avatar_url}} style={{width:75}}/>` },
+    {headerName: 'id', field: 'id'},
+    {headerName: 'name', field: 'name'},
+    {headerName: 'login', field: 'login'}
+  ]
   
 
-  initializeM = () => { 
+  initializeM = () => {
     console.log(this.props);
     this.setState((state, props) => {
-      // console.log({...state});
-      // console.log(props);
       return {names: props.names}
     })
     console.log(this.state);
     this.props.names.map(name => {
       this.updateStatewithProfiles(name);
     });
-    // this.updateStatewithProfiles
   }
 
   componentDidMount() {
@@ -50,7 +59,11 @@ export default class Card extends Component {
   }
 
   render(){
-    
+  //   const rowData = [
+  //      {make: "Toyota", model: "Celica", price: 35000},
+  //      {make: "Ford", model: "Mondeo", price: 32000},
+  //      {make: "Porsche", model: "Boxter", price: 72000}
+  //  ];
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -60,19 +73,27 @@ export default class Card extends Component {
             ref={this.enteredUserName}/>
           <button>enter</button>
         </form>
-        {/* { this.props.names.map(name => <h3>{name}</h3>) } */}
-        {this.state.profiles.map(profile => (
+        {/* {this.state.profiles.map(profile => (
          <div>
           <img src={profile.avatar_url} style={{width:75}}/>
           <div style={{display:'inline-block', margin: '0.5rem'}}>{profile.id}</div>
           <div style={{display:'inline-block', margin: '0.5rem'}}>{profile.name}</div>
-          <div style={{display:'inline-block', margin: '0.5rem'}}>{profile.login}</div>
-          {/* <h3>{profile.login}</h3>
-          <h3>{profile.id}</h3>
-          <h3>{profile.id}</h3> */}
-          
+          <div style={{display:'inline-block', margin: '0.5rem'}}>{profile.login}</div>       
          </div> 
-        ))}
+        ))} */}
+        <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+          <AgGridReact rowData={this.state.profiles}
+              defaultColDef={{
+                  initialWidth: 100,
+                  sortable: true,
+                  resizable: true}}
+              columnDefs={this.columnDefs}>
+            <AgGridColumn field="avatar_url"></AgGridColumn>
+            <AgGridColumn field="id"></AgGridColumn>
+            <AgGridColumn field="name"></AgGridColumn>
+            <AgGridColumn field="login"></AgGridColumn>
+          </AgGridReact>
+        </div>
       </div>
     );
   };
